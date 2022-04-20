bd8f6647cee85be12e28571a8471ad2caafec995
paopesce <paojpesce@gmail.com>
2022-04-20 08:53:20 -0300

    clase java

---
+++ script.js
@@ -0,0 +1,50 @@
+//esto es un comentario 
+/* esto es un comentario de mas de una linea 
+todo 
+es comentario
+*/
+//alert("Hola mundo")
+let alumno = "" //creo la variable
+let nota = 9
+
+const entidad = "Gobierno ciudad"
+
+alumno = "paola" //le cambio el contenido
+
+alumno = "emiliano"
+
+console.log("el nombre del alumno es " + alumno + " y su nota es " + nota) //la imprimo 
+
+//VAN
+let nombreApellido = "paola pesce"
+let User = "paola jesabel"
+let _usuario
+let $user
+let nombre_apellido = "Snake case"
+//let nombreApellido
+
+//no van
+/*let 1ernombre
+let nombre-apellido
+let var super break this*/
+
+////STRING
+let str1 = "JUAN"
+let str2 = 'pEDRO'
+let str4 = str1 + " y " + str2
+
+console.log (str4)
+
+///NUMBER
+let num1 = parseFloat(prompt("mete el primer numero"))
+let num2 = parseFloat(prompt("mete el segundo numero"))
+//operaciones basicas
+let suma = num1 + num2
+let resta = num1 - num2
+let mult = num1 * num2
+let division = num1/num2
+alert("La suma es:" +suma)
+alert("La resta es:" +resta)
+console.log("La multiplicacion es:" +mult)
+console.log("La division es:" +division)
+document.write("<h1> Javascript </h1>" + mult);
